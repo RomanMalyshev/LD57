5e68a01512a74e162680c6d31b385b2d36e14e7a
Roman <noagard@gmail.com>
2025-04-08 21:58:27 +0400

    objects

--- Assets/Megapoly.Art/Vintage Controls/Scripts/ButtonLamp.cs
+++ Assets/Megapoly.Art/Vintage Controls/Scripts/ButtonLamp.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ButtonLamp : MonoBehaviour
 {
@@ -15,7 +13,7 @@ public class ButtonLamp : MonoBehaviour
     public bool on;
     public Transform lamp;
     public eColor lightColor;
-
+    public Light light;
     Renderer rend;
     // Start is called before the first frame update
     void Start()
